2f58dfb41048013ca3b77413221bc57af77ff859
lukaszmazurkiewicz <manzi@o2.pl>
2019-05-29 09:29:06 +0200

    first commit

---
+++ src/main/java/com/kodilla/betapp_front/BetappFrontApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.betapp_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetappFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetappFrontApplication.class, args);
+    }
+
+}
